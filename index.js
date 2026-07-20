b749fd7933dbb8a4d58a442d6a353af70b115b7b
ryoooooooooooooooooooooo <c1514999@st.kanazawa-it.ac.jp>
2026-07-20 20:49:24 +0900

    課題の一覧を見れるようにuiを作った

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import session from 'express-session';
 import routes from './routes/index.js';
-import { initializeDatabase } from './sqlite/setup.js';
+import { getHomeworkList, initializeDatabase } from './sqlite/setup.js';
 
 initializeDatabase();
 
@@ -14,8 +14,21 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
+app.use(express.static('ui'));
+
+app.get('/api/homework', (req, res) => {
+
+try{
+  const data= getHomeworkList();
+  res.json(data);
+}catch(error){
+  res.status(500).json({ error: "DBエラー"});
+}
+});
+
 app.use('/', routes);
 
 app.listen(3000, () => {
   console.log("http://127.0.0.1:3000");
-})
+});
